b31029c15529e23f69722aed62b77ed6ff52473f
LordCheddar <lordcheddar@gmail.com>
2018-09-28 22:50:23 +0200

    add vatomview display face in engaged mode

--- app/src/main/java/io/blockv/example/feature/activated/ActivatedScreenImpl.java
+++ app/src/main/java/io/blockv/example/feature/activated/ActivatedScreenImpl.java
@@ -1,6 +1,7 @@
 package io.blockv.example.feature.activated;
 
 import android.view.LayoutInflater;
+import android.widget.TextView;
 import io.blockv.common.model.Vatom;
 import io.blockv.common.util.Cancellable;
 import io.blockv.common.util.CompositeCancellable;
@@ -14,13 +15,19 @@ import timber.log.Timber;
 public class ActivatedScreenImpl extends BaseScreen implements ActivatedScreen {
 
   final android.support.v7.widget.Toolbar toolbar;
+  final TextView name;
+  final TextView description;
   final VatomView icon;
+  final VatomView engaged;
   final VatomView card;
 
   public ActivatedScreenImpl(ActivatedActivity activity) {
     super(activity);
     toolbar = activity.findViewById(R.id.toolbar);
+    name = activity.findViewById(R.id.name);
+    description = activity.findViewById(R.id.description);
     icon = activity.findViewById(R.id.icon);
+    engaged = activity.findViewById(R.id.engaged);
     card = activity.findViewById(R.id.card);
 
     activity.setSupportActionBar(toolbar);
@@ -37,13 +44,13 @@ public class ActivatedScreenImpl extends BaseScreen implements ActivatedScreen {
 
   @Override
   public Cancellable setVatom(Vatom vatom) {
-    LayoutInflater inflater = LayoutInflater.from(activity);
+    name.setText(vatom.getProperty().getTitle());
+    description.setText(vatom.getProperty().getDescription());
 
     CompositeCancellable cancellable = new CompositeCancellable();
     cancellable.add(faceManager
       .load(vatom)
-      .setLoaderView(inflater.inflate(R.layout.view_vatom_loader, icon,false))
-      .setErrorView(inflater.inflate(R.layout.view_vatom_error, icon,false))
+      .setErrorView(LayoutInflater.from(activity).inflate(R.layout.view_basic_error, icon, false))
       .into(icon)
       .call(faceView -> {
         Timber.e(faceView.toString());
@@ -52,9 +59,16 @@ public class ActivatedScreenImpl extends BaseScreen implements ActivatedScreen {
       }));
     cancellable.add(faceManager
       .load(vatom)
+      .setEmbeddedProcedure(FaceManager.EmbeddedProcedure.ENGAGED)
+      .into(engaged)
+      .call(faceView -> {
+        Timber.e(faceView.toString());
+      }, throwable -> {
+        Timber.e(throwable.getMessage());
+      }));
+    cancellable.add(faceManager
+      .load(vatom)
       .setEmbeddedProcedure(FaceManager.EmbeddedProcedure.CARD)
-      .setLoaderView(inflater.inflate(R.layout.view_vatom_loader, card,false))
-      .setErrorView(inflater.inflate(R.layout.view_vatom_error, card,false))
       .into(card)
       .call(faceView -> {
         Timber.e(faceView.toString());
